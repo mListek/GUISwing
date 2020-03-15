475270e1f58f9379643c47329c3798bb350153eb
Michał Listek <michal.listex@gmail.com>
2020-03-15 15:42:47 +0100

    created login layout

--- src/GUISwing.java
+++ src/GUISwing.java
@@ -4,11 +4,35 @@ public class GUISwing {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
-        frame.setSize(100, 100);
+        JPanel panel = new JPanel();
+        frame.setSize(350, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25 );
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField();
+        userText.setBounds(100, 20 ,165, 25);
+        panel.add(userText);
+
+        JLabel passwordLable = new JLabel("Password");
+        passwordLable.setBounds(10, 50, 80, 25);
+        panel.add(passwordLable);
+
+        JPasswordField passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        JButton button = new JButton("Login");
+        button.setBounds(10, 80, 80, 25);
+        panel.add(button);
+
         frame.setVisible(true);
 
-        JPanel panel = new JPanel();
 
 
     }
